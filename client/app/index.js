b4f454f9276d3fe5dad1e004853c0cbc08e9ccad
railsr <ikll7@yahoo.com>
2016-09-25 04:35:05 +0400

    fixed base client

---
+++ client/app/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import HelloWorld from './components';
+
+render(<HelloWorld/>, document.getElementById('app'));
