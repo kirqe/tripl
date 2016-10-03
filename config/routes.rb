eba413c79aef7cfbc77c78fd11257245f1425952
railsr <ikll7@yahoo.com>
2016-10-03 23:29:16 +0400

    fefactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   root "admin/dashboard#index"
-  
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  constraints subdomain: 'api' do
-    scope module: 'api' do
+    scope module: 'api', constraints: { subdomain: 'api' } do
       namespace :v1 do
         resources :offers, only: [:index, :show, :create]
         get '/locations', to: "offers#locations" # list of coords for map
       end
     end
-  end
-  
+
 end
